89a35dafbb3f4f60380de60a632702a45fc5ce7f
Cary Goltermann <c@sumble.com>
2025-04-03 10:37:09 -0400

    Shorter sleep

--- sleep_flow.py
+++ sleep_flow.py
@@ -5,5 +5,5 @@ from prefect import flow
 @flow(log_prints=True)
 def sleep_flow():
     print("I'm tired, grandpa. Sleeping now.")
-    time.sleep(300)
+    time.sleep(60)
     print("Done sleeping. So well rested.")
